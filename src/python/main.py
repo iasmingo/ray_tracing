8be9d1f294676d612833060913006072c6517ab9
caionsantos <137624365+caionsantos@users.noreply.github.com>
2025-02-18 17:51:50 -0300

    teste com mais triângulos

--- src/python/main.py
+++ src/python/main.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 def main():
     # Configuração da câmera
-    camera_pos = Point3(0, 0, 0)
+    #camera_pos = Point3(0, 0, 0)
+    camera_pos = Point3(0, 0, 3)
     camera_mira = Point3(0, 0, -1)
     camera_up = Vector3(0, 1, 0)
     distancia_tela = 1.0
@@ -17,12 +18,13 @@ def main():
 
     #Cena inicial
     # esfera1 = Sphere(Point3(0, 0, -3), 1, Color.RED) 
-    esfera2 = Sphere(Point3(2, 0, -4), 1, Color.GREEN)  
+    #esfera2 = Sphere(Point3(2, 0, -4), 1, Color.GREEN)  
+    #mesh = TriangleMesh(1, 3, [Point3(0, 0, -3), Point3(1, -1, -4), Point3(2, 0, -3)], [(0, 1, 2)], [Vector3(0, -2, 2)], [], Color.RED)
     plano = Plane(Point3(0, -1, 0), Vector3(0, 1, 0), Color(0.5, 0.5, 0.5)) 
 
     # objetos = [esfera1, esfera2, plano]
 
-    mesh = TriangleMesh(1, 3, [Point3(0, 0, -3), Point3(1, -1, -4), Point3(2, 0, -3)], [(0, 1, 2)], [Vector3(0, -2, 2)], [], Color.RED)
+    mesh = TriangleMesh(2, 4, [Point3(0, 0, -3), Point3(1, -1, -4), Point3(2, 0, -3), Point3(1, 1, -2)], [(0, 1, 2), (0, 2, 3)], [Vector3(0, -2, 2), Vector3(0, 2, -2)], [], Color.RED)
     # esfera1 = Sphere(Point3(0, 0, -3), 1, Color.RED)
     objetos = [mesh, plano]
     #objetos = [esfera1]
