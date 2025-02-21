7a95481d0a87528c567b87ca0128cfa85ceac420
Iasmin <iasmin.ufpe@gmail.com>
2025-02-21 14:41:41 -0300

    feat: cena com plano e pirâmide

--- src/python/main.py
+++ src/python/main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 def main():
     # Configuração da câmera
-    #camera_pos = Point3(0, 0, 0)
     camera_pos = Point3(0, 0, 3)
     camera_mira = Point3(0, 0, -1)
     camera_up = Vector3(0, 1, 0)
@@ -16,18 +15,44 @@ def main():
 
     camera = Camera(camera_pos, camera_mira, camera_up, distancia_tela, altura_resolucao, largura_resolucao)
 
-    #Cena inicial
-    # esfera1 = Sphere(Point3(0, 0, -3), 1, Color.RED) 
-    #esfera2 = Sphere(Point3(2, 0, -4), 1, Color.GREEN)  
-    #mesh = TriangleMesh(1, 3, [Point3(0, 0, -3), Point3(1, -1, -4), Point3(2, 0, -3)], [(0, 1, 2)], [Vector3(0, -2, 2)], [], Color.RED)
-    plano = Plane(Point3(0, -1, 0), Vector3(0, 1, 0), Color(0.5, 0.5, 0.5)) 
+    # Definição dos vértices (inclinação para o lado)
+    vertices = [
+        Point3(-0.5, -1, -3),  
+        Point3(1.5, -1, -3), 
+        Point3(0.5, 1, -3),    
+        Point3(0, -1, -5),    
+        Point3(-0.2, 2, -4)    
+    ]
 
-    # objetos = [esfera1, esfera2, plano]
+    # Definição dos triângulos (índices dos vértices)
+    triangulos = [
+        (0, 1, 2),  # Triângulo base
+        (0, 1, 3),
+        (1, 2, 4),
+        (2, 0, 4),
+        (0, 3, 4),
+        (1, 3, 4)
+    ]
 
-    mesh = TriangleMesh(2, 4, [Point3(0, 0, -3), Point3(1, -1, -4), Point3(2, 0, -3), Point3(1, 1, -2)], [(0, 1, 2), (0, 2, 3)], [Vector3(0, -2, 2), Vector3(0, 2, -2)], [], Color.RED)
-    # esfera1 = Sphere(Point3(0, 0, -3), 1, Color.RED)
-    objetos = [mesh, plano]
-    #objetos = [esfera1]
+    # Cores
+    cores = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.WHITE, Color.BLACK]
+
+    # Calcular as normais de cada triângulo
+    normais = []
+    for t in triangulos:
+        v0, v1, v2 = vertices[t[0]], vertices[t[1]], vertices[t[2]]
+        normal = (v1 - v0).cross(v2 - v0).normalize()
+        normais.append(normal)
+
+    # Criar a malha triangular
+    piramide = TriangleMesh(len(triangulos), len(vertices), vertices, triangulos, normais, [], cores[0])
+
+    # Plano de fundo
+    plano = Plane(Point3(0, -1, 0), Vector3(0, 1, 0), Color(0.5, 0.5, 0.5))
+
+    # Lista de objetos na cena
+    objetos = [piramide, plano]
+
     # Renderizar a cena
     imagem = camera.draw(objetos)
 
@@ -36,6 +61,7 @@ def main():
     plt.axis('off')
     plt.savefig("image.png", bbox_inches='tight')
     plt.show()
+
 
 if __name__ == "__main__":
     main()
